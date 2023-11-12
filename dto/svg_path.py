fix loss and gradient

dto/svg_path.py
@@ -2,42 +2,54 @@ import drawsvg as draw
 import numpy as np
 from typing import List
 
-from dto.area import Area
 from dto.svg_segment import Segment, M, C
+from utils.image import get_area
 
 
 class SvgPath:
     path_arr: List[Segment]
-    color: np.array
-    gradient_color: draw.LinearGradient
+    colors: List[np.array]
     width: int
     height: int
     segments_count: int
 
-    def __init__(self, width: int, height: int, path_arr: List[Segment], color: np.array,
-                 gradient_color: draw.LinearGradient = None):
+    def __init__(self, width: int, height: int, path_arr: List[Segment], colors: List[np.array] = []):
         self.width = width
         self.height = height
         self.path_arr = path_arr
-        self.color = color
         self.segments_count = len(path_arr)
-        self.gradient_color = gradient_color
+        self.colors = colors
 
     def __copy__(self):
         path_arr = []
+        colors = []
         for segment in self.path_arr:
             path_arr.append(segment.__copy__())
-        return SvgPath(self.width, self.height, path_arr, np.array(self.color), self.gradient_color)
+        for color in self.colors:
+            colors.append(np.array(color))
+        return SvgPath(self.width, self.height, path_arr, colors)
 
     def set_path_arr(self, path_arr: np.array):
         self.path_arr = path_arr
         self.segments_count = len(path_arr)
 
-    def set_gradient_color(self, color1: np.array, color2: np.array, area: Area):
-        y = int((area.y1 - area.y0) / 2)
-        self.gradient_color = draw.LinearGradient(area.x0, y, area.x1, y)
-        self.gradient_color.add_stop(0, self.to_color(color1), 1)
-        self.gradient_color.add_stop(1, self.to_color(color2), 1)
+    def add_color(self, color: List[np.array]):
+        for c in color:
+            self.colors.append(c)
+
+    def get_avg_colors(self) -> np.array:
+        ans = []
+        a = []
+        colors_count = len(self.colors[0])
+        for color in self.colors:
+            color_arr = []
+            for c in color:
+                color_arr.append(c)
+            ans.append(color_arr)
+        ans = np.array(ans)
+        for i in range(colors_count):
+            a.append(sum(ans[:, i]) / len(ans))
+        return np.array(a)
 
     @staticmethod
     def to_color(color: np.array) -> str:
@@ -46,10 +58,15 @@ class SvgPath:
     def create_drawing_object(self) -> draw.Path:
         assert self.path_arr is not None and len(self.path_arr) != 0
 
-        if self.gradient_color is None:
-            c = self.to_color(self.color)
+        if len(self.colors) == 1:
+            c = self.to_color(self.colors[0])
         else:
-            c = self.gradient_color
+            area = get_area(self.path_arr, self.width, self.height)
+            size = len(self.colors) - 1
+            y = int((area.y1 - area.y0) / 2)
+            c = draw.LinearGradient(area.x0, y, area.x1, y)
+            for i, color in enumerate(self.colors):
+                c.add_stop(i / size, self.to_color(color), 1)
 
         path = draw.Path(
             fill=c,
@@ -60,14 +77,14 @@ class SvgPath:
 
         for segment in self.path_arr:
             if isinstance(segment, M):
-                path.M(segment.x * self.width, segment.y * self.height)
+                path.M(segment.x, segment.y)
             elif isinstance(segment, C):
-                c1x = float(segment.x * self.width)
-                c1y = float(segment.y * self.height)
-                c2x = float(segment.x1 * self.width)
-                c2y = float(segment.y1 * self.height)
-                ex = float(segment.x2 * self.width)
-                ey = float(segment.y2 * self.height)
+                c1x = segment.x
+                c1y = segment.y
+                c2x = segment.x1
+                c2y = segment.y1
+                ex = segment.x2
+                ey = segment.y2
                 path.C(c1x, c1y, c2x, c2y, ex, ey)
 
         return path

main.py
@@ -43,9 +43,12 @@ def create_children(cur_population: List[SvgPicture]) -> List[SvgPicture]:
         paths = []
         for path in first_parent.paths:
             path_arr = []
+            colors = []
             for segment in path.path_arr:
                 path_arr.append(segment.__copy__())
-            paths.append(SvgPath(path.width, path.height, path_arr, np.array(path.color), path.gradient_color))
+            for color in path.colors:
+                colors.append(np.array(color))
+            paths.append(SvgPath(path.width, path.height, path_arr, colors))
         child = SvgPicture(paths, config.PNG_PATH)
         children.append(child)
     if config.DEBUG:

mutations/concat_path.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from dto.svg_picture import SvgPicture
 from mutations.base import Mutation
-from utils.image import get_area
 
 
 class ConcatPath(Mutation):
@@ -21,14 +20,12 @@ class ConcatPath(Mutation):
         random_path_index2 = random.randint(0, len(picture.paths) - 1)
         path1 = picture.paths[random_path_index1]
         path2 = picture.paths[random_path_index2]
-        diff_color = abs(np.sum(np.subtract(path1.color, path2.color)))
-        if len(picture.paths) > 1 and diff_color <= self.diff_color and random_path_index1 != random_path_index2 \
-                and path1.gradient_color is None and path2.gradient_color is None:
+        diff_color = abs(np.sum(np.subtract(path1.get_avg_colors(), path2.get_avg_colors())))
+        if len(picture.paths) > 1 and diff_color <= self.diff_color and random_path_index1 != random_path_index2:
             new_path = path1.path_arr.copy()
             for segment in path2.path_arr:
                 new_path.append(segment)
             path1.set_path_arr(new_path)
-            area = get_area(path1, picture.width, picture.height)
-            path1.set_gradient_color(path1.color, path2.color, area)
+            path1.add_color(path2.colors)
             picture.del_path(random_path_index2)
         return picture

utils/image.py
@@ -1,11 +1,10 @@
 import numpy as np
 from PIL import Image
 import cv2
-import typing
+from typing import List, Sequence
 
 from dto.area import Area
-from dto.svg_path import SvgPath
-from dto.svg_segment import M, C
+from dto.svg_segment import M, C, Segment
 
 
 def read_picture(path: str) -> np.array:
@@ -18,7 +17,7 @@ def read_picture(path: str) -> np.array:
     return np.array(new_image)
 
 
-def get_contours(pic: np.array) -> typing.Sequence[cv2.UMat]:
+def get_contours(pic: np.array) -> Sequence[cv2.UMat]:
     pic = pic[:, :, ::-1]
     gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -31,20 +30,20 @@ def get_contours(pic: np.array) -> typing.Sequence[cv2.UMat]:
 
 
 # Получение площади прямоугольной области в которой расположен путь
-def get_area(path: SvgPath, width: int, height: int) -> Area:
+def get_area(path_arr: List[Segment], width: int, height: int) -> Area:
     max_x = 0
     min_x = width
     max_y = 0
     min_y = height
-    for segment in path.path_arr:
+    for segment in path_arr:
         x = 0
         y = 0
         if isinstance(segment, M):
-            x = segment.x * width
-            y = segment.y * height
+            x = segment.x
+            y = segment.y
         elif isinstance(segment, C):
-            x = segment.x2 * width
-            y = segment.y2 * height
+            x = segment.x2
+            y = segment.y2
         max_x = max(max_x, x)
         min_x = min(min_x, x)
         max_y = max(max_y, y)

utils/loss.py
@@ -23,17 +23,18 @@ def opt_transport_loss(png_cnt, cur_cnt):
 
 
 def image_diff(png_first, png_second) -> float:
-    image_first = np.array(Image.open(png_first).convert('RGBA'))
-    image_second = np.array(Image.open(png_second).convert('RGBA'))
+    image_first = np.array(Image.open(png_first).convert('RGB'), dtype=int)
+    image_second = np.array(Image.open(png_second).convert('RGB'), dtype=int)
     return float(abs(np.sum(image_second - image_first)))
 
 
 def image_diff_exp(png_first, png_second) -> float:
-    image_first = np.array(Image.open(png_first).convert('RGBA'))
-    image_second = np.array(Image.open(png_second).convert('RGBA'))
+    image_first = np.array(Image.open(png_first).convert('RGB'), dtype=int)
+    image_second = np.array(Image.open(png_second).convert('RGB'), dtype=int)
     return float(np.sum(np.exp((abs(image_second - image_first) / 255) * 50 + 1)))
 
+
 def image_diff_mse(png_first, png_second) -> float:
-    image_first = np.array(Image.open(png_first).convert('RGBA'))
-    image_second = np.array(Image.open(png_second).convert('RGBA'))
+    image_first = np.array(Image.open(png_first).convert('RGB'), dtype=int)
+    image_second = np.array(Image.open(png_second).convert('RGB'), dtype=int)
     return float(np.sum(np.power((abs(image_second - image_first) / 255), 2)))

vectorize_by_algo.py
@@ -21,10 +21,10 @@ def complex_to_pair(n):
         data = str(n).split('-')
     if len(data) == 1:
         if data[0].__contains__("j"):
-            return 0, float(data[0][:-1])
+            return 0, round(float(data[0][:-1]), 1)
         else:
-            return float(data[0]), 0
-    return float(data[0][1:]), float(data[1][:-2])
+            return round(float(data[0]), 1), 0
+    return round(float(data[0][1:]), 1), round(float(data[1][:-2]), 1)
 
 
 def get_color(color) -> np.array:
@@ -47,15 +47,15 @@ def preprocess_svg_paths(svg_path, png_file_path: str, w: int, h: int) -> SvgPic
             if is_first:
                 is_first = False
                 p1, p2 = complex_to_pair(curve.start)
-                new_curve.append(M(p1 / w, p2 / h))
+                new_curve.append(M(p1, p2))
             if isinstance(curve, Line):
                 p1, p2 = complex_to_pair(curve.start)
                 p3, p4 = complex_to_pair(curve.end)
 
                 if last_coord is not None and (p1 != last_coord[0] or p2 != last_coord[1]):
-                    new_curve.append(M(p1 / w, p2 / h))
+                    new_curve.append(M(p1, p2))
 
-                new_curve.append(C(p1 / w, p2 / h, p3 / w, p4 / h, p3 / w, p4 / h))
+                new_curve.append(C(p1, p2, p3, p4, p3, p4))
                 last_coord = p3, p4
             elif isinstance(curve, QuadraticBezier):
                 p1, p2 = complex_to_pair(curve.start)
@@ -63,13 +63,13 @@ def preprocess_svg_paths(svg_path, png_file_path: str, w: int, h: int) -> SvgPic
                 p5, p6 = complex_to_pair(curve.control)
 
                 if last_coord is not None and (p1 != last_coord[0] or p2 != last_coord[1]):
-                    new_curve.append(M(p1 / w, p2 / h))
+                    new_curve.append(M(p1, p2))
 
-                new_curve.append(C(p1 / w, p2 / h, p3 / w, p4 / h, p5 / w, p6 / h))
+                new_curve.append(C(p1, p2, p5, p6, p3, p4))
                 last_coord = p3, p4
             else:
                 print(f'unkhown curve = {curve}')
-        new_paths.append(SvgPath(path_arr=new_curve, width=width, height=height, color=get_color(attr['fill'])))
+        new_paths.append(SvgPath(path_arr=new_curve, width=width, height=height, colors=[get_color(attr['fill'])]))
     return SvgPicture(new_paths, png_file_path)
 
 